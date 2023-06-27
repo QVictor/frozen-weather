7caadc93c31ccc2a0f87acd8dffc0f35bc3b5aed
qVictor <qqlogin@yandex.ru>
2023-06-27 07:33:21 +0300

    add data in country and city tables

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        City::insert([
+            [
+                'name' => 'Moscow',
+                'open_weather_city_id' => 524901,
+                'country_code' => 'RU'
+            ],
+            [
+                'name' => 'Saint Petersburg',
+                'open_weather_city_id' => 498817,
+                'country_code' => 'RU'
+            ],
+            [
+                'name' => 'London',
+                'open_weather_city_id' => 2643741,
+                'country_code' => 'GB'
+            ]
+        ]);
     }
 }
