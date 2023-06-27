7615f0781842ddea3669285a927b96a051cc64a1
qVictor <qqlogin@yandex.ru>
2023-06-27 07:43:33 +0300

    add relationships

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Country extends Model
 {
@@ -12,4 +13,9 @@ class Country extends Model
     protected $table = 'countries';
 
     protected $primaryKey = 'two_letters_code';
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class, 'country_code', 'two_letters_code');
+    }
 }
--- app/Models/OpenWeatherData.php
+++ app/Models/OpenWeatherData.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OpenWeatherData extends Model
 {
@@ -20,4 +21,9 @@ class OpenWeatherData extends Model
         'Clear',
         'Clouds',
     ];
+
+    public function city():BelongsTo
+    {
+        return $this->belongsTo(OpenWeatherData::class);
+    }
 }
